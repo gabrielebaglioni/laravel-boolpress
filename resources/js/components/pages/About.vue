7b6521e9526306e1086ecc72191d0cbf615253cf
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-27 17:30:42 +0200

    add About

--- resources/js/components/pages/About.vue
+++ resources/js/components/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="conteiner">
      <h1>chi siamo</h1>
-     <p>òLorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, necessitatibus voluptatum. Eos dolor voluptates impedit laboriosam, maiores ipsa vitae id explicabo veritatis perferendis soluta repellendus iure, amet, recusandae nostrum quam.</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, necessitatibus voluptatum. Eos dolor voluptates impedit laboriosam, maiores ipsa vitae id explicabo veritatis perferendis soluta repellendus iure, amet, recusandae nostrum quam.</p>
   </div>
 </template>
 
